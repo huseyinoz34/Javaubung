814999da07185eb108605a526ed0aea4b075c4ba
hüseyin <hseyinzdemir500334@gmail.com>
2022-09-01 22:47:31 +0200

    Initial commit

---
+++ src/C001.java
@@ -0,0 +1,11 @@
+public class C001 {
+
+    public static void main(String[] args) {
+        for (int satir=1;satir <=5;satir++){
+            for (int sutun=1;sutun<=5;sutun++){
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
